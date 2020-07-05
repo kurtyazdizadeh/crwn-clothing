48c5040945f0449c28524f9c727b4b6bb9843bb9
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-07-05 14:00:57 -0700

    added sass, created homepage component, wrote skeleton for menu tile in homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './components/homepage';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+    <div className='App'>
+      <HomePage />
+    </div>
+    )
   }
 }
 
