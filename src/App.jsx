94f43242b37f2a8dd8d7ce3b7fe6b6f7056a1d50
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-07-06 15:05:50 -0700

    added header component, added svg, links for navigation and styling for header

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
-
+import Header from './components/header/header';
 
 class App extends Component {
   render() {
     return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
