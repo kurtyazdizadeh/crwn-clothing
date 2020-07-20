bf3b52b4aeb038d05961b351b46a7e0672d87511
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-07-20 16:01:19 -0700

    added stripe integration, changed fav.ico

---
+++ src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51H77YMBnxoxxdMSrLUf1ealf2JF5bRz5UFtZZK1z9t6cQUbxQZkSFOSnUL2SYj0mBTK0qVzoX2zCp31l03SSK2hx00QQ5xJ7WJ';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+};
+
+export default StripeCheckoutButton;
