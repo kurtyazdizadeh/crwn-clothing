7f28331a3d1e7f960c9f1fa76075230ffa1e3e2f
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-08-04 12:36:56 -0700

    added redux-sagas to replace redux-thunk, created saga for shop page, refactored fetchCollectionsStart action for shop into a saga, created rootSaga to plan ahead for other sagas to be added later

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 
-const middlewares = [logger, thunk];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware, logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-export const persistor = persistStore(store);
 
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
 export default { store, persistor };
